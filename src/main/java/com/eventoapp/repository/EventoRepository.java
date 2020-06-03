0e690e65559ef8199f323096486dcc13b053d33e
C1311207 Jaqueline Palmeira Pedrosa <c1311207@interno.bb.com.br>
2020-06-03 11:45:22 -0300

    restaurando os arquivos

---
+++ src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -0,0 +1,8 @@
+package com.eventoapp.eventoapp.repository;
+
+import com.eventoapp.eventoapp.model.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento,String> {
+    Evento findByCodigo(long codigo);
+}
